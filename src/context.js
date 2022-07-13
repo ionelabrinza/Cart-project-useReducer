f8c3bc793a055508e0564b322aaafe39a6ebf6b7
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-07-12 22:11:45 -0500

    remove single item

--- src/context.js
+++ src/context.js
@@ -19,11 +19,16 @@ const AppProvider = ({ children }) => {
     dispatch({ type: 'CLEAR_CART' });
   };
 
+  const remove = (id) => {
+    dispatch({ type: 'REMOVE', payload: id });
+  };
+
   return (
     <AppContext.Provider
       value={{
         ...state,
         clearCart,
+        remove,
       }}
     >
       {children}
