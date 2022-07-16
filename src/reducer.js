cab1f6ae0c43d6011d1f34c3882ce9f9bd90958e
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-07-16 13:27:55 -0500

    fetch data

--- src/reducer.js
+++ src/reducer.js
@@ -46,7 +46,12 @@ const reducer = (state, action) => {
     total = parseFloat(total.toFixed(2));
     return { ...state, total, amount };
   }
-
+  if (action.type === 'LOADING') {
+    return { ...state, loading: true };
+  }
+  if (action.type === 'DISPLAY_ITEMS') {
+    return { ...state, cart: action.payload, loading: false };
+  }
   return state;
 };
 
