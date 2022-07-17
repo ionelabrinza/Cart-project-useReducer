bf0124418bc683f0fc8bd6f1cc87941be7346871
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-07-17 09:26:50 -0500

    toggle amount

--- src/context.js
+++ src/context.js
@@ -15,29 +15,28 @@ const initialState = {
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
   const clearCart = () => {
     dispatch({ type: 'CLEAR_CART' });
   };
-
   const remove = (id) => {
     dispatch({ type: 'REMOVE', payload: id });
   };
-
   const increase = (id) => {
     dispatch({ type: 'INCREASE', payload: id });
   };
-
   const decrease = (id) => {
     dispatch({ type: 'DECREASE', payload: id });
   };
-
   const fetchData = async () => {
     dispatch({ type: 'LOADING' });
     const response = await fetch(url);
     const cart = await response.json();
     dispatch({ type: 'DISPLAY_ITEMS', payload: cart });
   };
-
+  const toggleAmount = (id, type) => {
+    dispatch({ type: 'TOGGLE_AMOUNT', payload: { id, type } });
+  };
   useEffect(() => {
     fetchData();
   }, []);
@@ -53,6 +52,7 @@ const AppProvider = ({ children }) => {
         remove,
         increase,
         decrease,
+        toggleAmount,
       }}
     >
       {children}
--- src/reducer.js
+++ src/reducer.js
@@ -44,6 +44,7 @@ const reducer = (state, action) => {
       }
     );
     total = parseFloat(total.toFixed(2));
+
     return { ...state, total, amount };
   }
   if (action.type === 'LOADING') {
@@ -52,7 +53,23 @@ const reducer = (state, action) => {
   if (action.type === 'DISPLAY_ITEMS') {
     return { ...state, cart: action.payload, loading: false };
   }
-  return state;
+  if (action.type === 'TOGGLE_AMOUNT') {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === 'inc') {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+          if (action.payload.type === 'dec') {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+  throw new Error('no matching action type');
 };
 
 export default reducer;
